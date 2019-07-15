de60385ab7346b5b38cf6da1bbb02246cf15f6dc
AhmadNosratian <nosratian.ahmad@gmail.com>
2019-07-15 10:32:59 +0430

    view model provider depency added

--- app/src/main/java/ir/ahmadnosratian/contact/ViewModelProviderFactory.java
+++ app/src/main/java/ir/ahmadnosratian/contact/ViewModelProviderFactory.java
@@ -1,4 +1,35 @@
 package ir.ahmadnosratian.contact;
 
-public class ViewModelProviderFactory {
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import ir.ahmadnosratian.contact.data.DataManager;
+import ir.ahmadnosratian.contact.utils.rx.SchedulerProvider;
+import ir.ahmadnosratian.contact.view.ContactViewModel;
+
+@Singleton
+public class ViewModelProviderFactory extends ViewModelProvider.NewInstanceFactory {
+
+    private DataManager dataManager;
+    private SchedulerProvider schedulerProvider;
+
+    @Inject
+    public ViewModelProviderFactory(DataManager dataManager, SchedulerProvider schedulerProvider) {
+        this.dataManager = dataManager;
+        this.schedulerProvider = schedulerProvider;
+    }
+
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(ContactViewModel.class)) {
+            return (T) new ContactViewModel(dataManager, schedulerProvider);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel Class " + modelClass.getName());
+    }
 }
